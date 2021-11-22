82ccd7405cec74b33ae29a5e55e27a2951d6cdfe
Ginea Merrill <gineakanoe@gmail.com>
2021-11-22 11:15:26 -0500

    did some configuring with the sidebar so now all the components route with the links instead of all being on the App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
-import Sidebar from './components/Router/Sidebar';
-import Auth from './components/Auth/Auth';
-import Tales from './components/Tales/Tales';
-import Quests from './components/Quests/Quests';
+import Sidebar from './components/Site/Sidebar';
+// import Auth from './components/Auth/Auth';
+// import Tales from './components/Tales/Tales';
+// import Quests from './components/Quests/Quests';
 
 const App: React.FunctionComponent = () => {
 
     return (
         <div className="App">
-            <Auth />
-            <br />
-            <Tales />
-            <br/>
-            <Quests />
             <Router>
                 <Sidebar />
             </Router>
--- src/components/Site/Sidebar.tsx
+++ src/components/Site/Sidebar.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import {Route, Link, Routes} from 'react-router-dom';
+import './site.css';
 import Quests from "../Quests/Quests";
 import Tales from "../Tales/Tales";
+import Auth from '../Auth/Auth';
 
 const Sidebar: React.FunctionComponent = () => {
 
     return(
         <div className='sidebar'>
             <div>
-                <ul>
+                <ul id='sidebarUl'>
+                    <li><Link to='/auth'>Login/Register/Logout</Link></li>
                     <li><Link to='/tales'>Tales</Link></li>
                     <li><Link to='quests'>Quests</Link></li>
                 </ul>
             </div>
             <div>
                 <Routes>
+                    <Route path='/auth' element={<Auth />} />
                     <Route path='/tales' element={<Tales />} />
                     <Route path='/quests' element={<Quests />} />
                 </Routes>

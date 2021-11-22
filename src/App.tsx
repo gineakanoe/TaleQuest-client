4d75d9f54bd972eb9452f2f3725a0c915d26303d
Ginea Merrill <gineakanoe@gmail.com>
2021-11-22 11:31:03 -0500

    added basic Header and changed tab title to TaleQuest

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
+import Header from './components/Site/Header';
 import Sidebar from './components/Site/Sidebar';
 import Footer from './components/Site/Footer';
 // import Auth from './components/Auth/Auth';
@@ -11,6 +12,8 @@ const App: React.FunctionComponent = () => {
 
     return (
         <div className="App">
+            <Header />
+            
             <Router>
                 <Sidebar />
             </Router>
---
+++ src/components/Site/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header: React.FunctionComponent = () => {
+
+    return(
+        <div>
+            <h1>TaleQuest</h1>
+        </div>
+    )
+}
+
+export default Header;
